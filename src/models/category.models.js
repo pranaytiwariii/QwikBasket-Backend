e50afa5514e95632708136642a99899a873a6e0b
Gurveer2711 <singhgurveer337@gmail.com>
2025-09-20 00:09:36 +0530

    feat: add category, product, and subcategory models

---
+++ src/models/category.models.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+const CategorySchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true }, // Catergory name
+  image: { type: String, required: true }, // URL for the category image
+});
+
+export default mongoose.model("Category", CategorySchema);
